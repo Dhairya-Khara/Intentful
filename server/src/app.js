9ed98af6aa44a2e8f021cf9cb49ac6a4c7efa9ef
Dhairya Khara <kharadhairya@gmail.com>
2022-10-03 10:57:13 -0400

    added cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,23 +1,28 @@
 const express = require('express')
 const multer = require('multer')
+const cors = require('cors')
+
 const app = express()
+app.use(cors())
 
 app.get('/', (req, res) =>{
     res.send('aidan is the best')
 })
 
+// configuring file upload
 const storage = multer.diskStorage({
     destination: (req, file, cb)=>{
         cb(null, __dirname)
     },
     filename: (req, file, cb) =>{
-        cb(null, file.fieldname )
+        console.log(file)
+        cb(null, file.originalname )
     }
 })
 
 const upload = multer({storage: storage})
 
-
+// route for file upload
 app.post('/upload', upload.single('file'), (req, res)=>{
     res.send()
 })
