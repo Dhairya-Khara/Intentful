44fd37373223920997dce02761236cfaca54986f
Dhairya Khara <kharadhairya@gmail.com>
2022-10-03 09:41:41 -0400

    creating endpoint to upload file

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,27 @@
 const express = require('express')
+const multer = require('multer')
 const app = express()
 
 app.get('/', (req, res) =>{
     res.send('aidan is the best')
 })
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb)=>{
+        cb(null, __dirname)
+    },
+    filename: (req, file, cb) =>{
+        cb(null, file.fieldname )
+    }
+})
+
+const upload = multer({storage: storage})
+
+
+app.post('/upload', upload.single('file'), (req, res)=>{
+    res.send()
+})
+
 
 
 app.listen(8080, ()=>{
