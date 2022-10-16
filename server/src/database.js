02f7aa4ae3055526f025ec1b6b353cdd44e66e4b
Dhairya Khara <kharadhairya@gmail.com>
2022-10-16 12:49:28 -0400

    updated schema

--- server/src/database.js
+++ server/src/database.js
@@ -3,10 +3,24 @@ const mongoose = require('mongoose')
 const connectionURL = "mongodb://127.0.0.1:27017/transcripts"
 
 let Schema = new mongoose.Schema({
-    transcript: {
-        type: Buffer
+    emaill: {
+        type: String
+    },
+    password: {
+        type: String
+    },
+    tokens: [{ // required for auth, dw about it
+
+    }],
+    transcripts: [ // to store raw transcripts files
+
+    ],
+    intents: { // model Berke made
+
     }
+
 })
+
 
 let Item = mongoose.model('Item', Schema)
 
