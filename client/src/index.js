86ea0647d712f95b45881f16cd85b99a5dad6171
Dhairya Khara <kharadhairya@gmail.com>
2022-10-30 21:21:10 -0400

    redux persist implemented

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from "react-redux";
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
 
 import AppRouter from './AppRouter';
 import store from "./redux/configureStore"
 import "./styles/App.css"
 
+const persistor = persistStore(store)
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <React.StrictMode>
-      <AppRouter />
+      <PersistGate loading={null} persistor={persistor}>
+        <AppRouter />
+      </PersistGate>
     </React.StrictMode>
   </Provider>
 );
--- client/src/redux/configureStore.js
+++ client/src/redux/configureStore.js
@@ -1,9 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit'
+import storage from 'redux-persist/lib/storage'
+import { persistReducer } from 'redux-persist'
+
 
 import authReducer from "./authReducer"
 
+const persistConfig = {
+    key: 'root',
+    storage
+};
+
+const persistedReducer = persistReducer(persistConfig, authReducer);
+
 export default configureStore({
     reducer: {
-        auth: authReducer
+        auth: persistedReducer
     }
 })
