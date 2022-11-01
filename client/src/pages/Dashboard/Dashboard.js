1a8fa50600348df0da6374d22eab04048d96eb28
Dhairya Khara <kharadhairya@gmail.com>
2022-10-31 20:49:33 -0400

    not rendering bubbles temporarily

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -3,24 +3,26 @@ import { useSelector } from 'react-redux'
 import FileUploader from './components/FileUploader'
 import Logout from './components/Logout'
 import BubbleChart from './components/BubbleChart'
+import FileViewer from "./components/FileViewer"
 
 export default function Dashboard() {
     const auth = useSelector((state) => state.auth.authenticated)
-    if(auth){
+    if (auth) {
         return (
             <div>
                 <Logout />
                 <h1>Intentful</h1>
                 <h3>By the order of Techy Blinders</h3>
                 <FileUploader />
-                <BubbleChart />
+                <FileViewer />
+                {/* <BubbleChart /> */}
             </div>
         )
     }
-    else{
-        return(
-        <div>
-            <h3>403 - Not Authenticated</h3>
-        </div>)
+    else {
+        return (
+            <div>
+                <h3>403 - Not Authenticated</h3>
+            </div>)
     }
 }
