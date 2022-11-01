acf99706fa6c58e9777b3478de5fa0269f18044f
Dhairya Khara <kharadhairya@gmail.com>
2022-10-31 20:50:38 -0400

    clean up

--- client/src/pages/Dashboard/components/FileUploader.js
+++ client/src/pages/Dashboard/components/FileUploader.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { useSelector } from 'react-redux'
 
@@ -6,7 +6,6 @@ import { useSelector } from 'react-redux'
 export default function FileUploader() {
     const [selectedFile, setSelectedFile] = useState(undefined)
     const [fileUploadStatus, setFileUploadStatus] = useState('')
-
     const token = useSelector((state) => state.auth.token)
 
     const onSubmit = (e) => {
@@ -23,55 +22,16 @@ export default function FileUploader() {
     return (
         <div>
             <form className="App" onSubmit={onSubmit}>
-                <input type="file" name="file" onChange= {e => setSelectedFile(e.target.files[0])} />
+                <input type="file" name="file" onChange={e => setSelectedFile(e.target.files[0])} />
                 <button>Upload</button>
             </form>
-            {/* {fileUploadStatus === "" ? "" : <Boxes />} */}
         </div>
     )
 
 }
 
-// class FileUploader extends React.Component {
-
-//     constructor() {
-//         super()
-//         this.state = {
-//             selectedFile: undefined,
-//             fileUploadStatus: ""
-//         }
-//     }
-
-//     handleUpload = (event) => {
-//         event.preventDefault();
-//         const data = new FormData();
-//         data.append('file', this.state.selectedFile, this.state.selectedFile.name);
-//         axios.post("http://localhost:8080/upload", data).then((res) => {
-//             this.setState({
-//                 fileUploadStatus: "File " + this.state.selectedFile.name + " has been upload successfully."
-//             })
-//         });
-//     }
-
-//     handleFileChange = (event) => {
-//         this.setState({
-//             selectedFile: event.target.files[0]
-//         })
-//     }
 
 
-//     render() {
-//         return (
-//             <div>
-                // <form className="App" onSubmit={this.handleUpload}>
-                //     <input type="file" name="file" onChange={this.handleFileChange} />
-                //     <button>Upload</button>
-                // </form>
-                // {this.state.fileUploadStatus === "" ? "" : <Boxes />}
-//             </div>
-//         )
-//     }
-// }
 
-// export default FileUploader
+
 
