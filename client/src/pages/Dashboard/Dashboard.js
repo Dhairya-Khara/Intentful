2eb0acb9121fbf25bf63f449ff8b10cd3a2d2d26
Dhairya Khara <kharadhairya@gmail.com>
2022-11-01 21:47:28 -0400

    fixed bug by squeezing everything into one component

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import FileUploader from './components/FileUploader'
 import Logout from './components/Logout'
 import BubbleChart from './components/BubbleChart'
-import FileViewer from "./components/FileViewer"
+import FileSystem from './components/FileSystem'
 
 export default function Dashboard() {
     const auth = useSelector((state) => state.auth.authenticated)
@@ -13,8 +12,9 @@ export default function Dashboard() {
                 <Logout />
                 <h1>Intentful</h1>
                 <h3>By the order of Techy Blinders</h3>
-                <FileUploader />
-                <FileViewer />
+                <FileSystem />
+                {/* <FileUploader />
+                <FileViewer /> */}
                 {/* <BubbleChart /> */}
             </div>
         )
--- client/src/pages/Dashboard/components/FileSystem.js
+++ client/src/pages/Dashboard/components/FileSystem.js
@@ -1,12 +1,30 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
 import { useSelector } from 'react-redux'
+import axios from 'axios'
+import File from './File'
+
+export default function FileSystem() {
 
 
-export default function FileUploader() {
+    const [files, setFiles] = useState([])
+    const token = useSelector((state) => state.auth.token)
     const [selectedFile, setSelectedFile] = useState(undefined)
     const [fileUploadStatus, setFileUploadStatus] = useState('')
-    const token = useSelector((state) => state.auth.token)
+
+    const getData = () => {
+        const url = "http://localhost:8080/getTranscripts"
+        let dataSoFar = []
+        axios.get(url, { headers: { "Authorization": "Bearer " + token } }).then(async (res) => {
+            for (const index in res.data) {
+                const filename = Object.keys(res.data[index])[0]
+                dataSoFar.push(filename)
+            }
+            setFiles(dataSoFar)
+        }).catch((error) => {
+            alert(error)
+        })
+
+    }
 
     const onSubmit = (e) => {
         e.preventDefault();
@@ -17,7 +35,15 @@ export default function FileUploader() {
         }).catch((error) => {
             alert("Not Authenticated")
         })
+        setFiles(oldFiles => [...oldFiles, selectedFile.name])
     }
+
+
+    useEffect(() => {
+        getData()
+    }, [])
+
+
 
     return (
         <div>
@@ -25,13 +51,8 @@ export default function FileUploader() {
                 <input type="file" name="file" onChange={e => setSelectedFile(e.target.files[0])} />
                 <button>Upload</button>
             </form>
+            <h2>Transcripts</h2>
+            {files === undefined ? <p>No Transcripts have been uploaded</p> : files.map((file) => <File key={file} name={file} />)}
         </div>
     )
-
 }
-
-
-
-
-
-
