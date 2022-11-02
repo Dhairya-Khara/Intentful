597f4d14a7b0143858af115348e09f5095c4aa05
Aidan <aidan.li@mail.utoronto.ca>
2022-11-01 22:19:26 -0400

    started visualizeData function to gen graph

--- client/src/pages/Dashboard/components/File.js
+++ client/src/pages/Dashboard/components/File.js
@@ -2,11 +2,14 @@ import axios from 'axios'
 import React from 'react'
 
 export default function File(props) {
+    const visualizeData = () => {
+        console.log('hello')
+    }
 
     return (
         <div className='singleFile'>
             <p>{props.name}</p>
-            <button>Visualize</button>
+            <button onClick={visualizeData}>Visualize</button>
         </div>
     )
 }
