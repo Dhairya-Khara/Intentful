4ce380e519c671d219033d10ab8c9653e0f40caa
krishnacheemalapati <krishkrishct@gmail.com>
2022-11-02 21:56:23 -0400

    Deleted unused code

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -13,9 +13,6 @@ export default function Dashboard() {
                 <h1>Intentful</h1>
                 <h3>By the order of Techy Blinders</h3>
                 <FileSystem />
-                {/* <FileUploader />
-                <FileViewer /> */}
-                {/* <BubbleChart /> */}
             </div>
         )
     }
