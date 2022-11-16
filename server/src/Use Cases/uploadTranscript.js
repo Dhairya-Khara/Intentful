3e1cb72f5ab7c6b8b7167a4d62276c7a78c21d93
Dhairya Khara <kharadhairya@gmail.com>
2022-11-16 00:43:37 -0500

    implemented all use cases

---
+++ server/src/Use Cases/uploadTranscript.js
@@ -0,0 +1,43 @@
+const processTranscript = require('./utils/transcriptProcessor')
+
+const uploadTranscriptInteractor = async (user, file, filename) => {
+    const json = JSON.parse(file)
+
+    let allCurrentIntents = new Map()
+
+    if (user.intents !== undefined) {
+        allCurrentIntents = new Map(Object.entries(user.intents));
+    }
+
+    //single transcript processing
+    let intentsForThisFile = processTranscript(new Map(), [json])
+
+    //multiple transcript processing
+    allCurrentIntents = processTranscript(allCurrentIntents, [json])
+
+    // save transcript
+    try {
+        const obj = {}
+        obj[filename] = file
+        obj["intents"] = intentsForThisFile
+        user.transcripts = user.transcripts.concat(obj)
+        await user.save()
+    }
+    catch (e) {
+        throw new Error("Error in saving transcripts", e)
+    }
+
+    // save intents
+    try {
+        user.intents = allCurrentIntents
+        await user.save()
+    }
+    catch (e) {
+        throw new Error("Error in saving intents", e)
+    }
+
+
+
+}
+
+module.exports = uploadTranscriptInteractor
---
+++ server/src/Use Cases/utils/transcriptProcessor.js
@@ -0,0 +1,49 @@
+function processTranscript(existingProcessedMap, transcript_json_list) {
+    let processedMap = new Map(existingProcessedMap)
+
+    // iterating all transcripts
+    for (const transcript_json of transcript_json_list) {
+
+        let prevIntent = undefined
+        // iterating each message in a single transcript
+        for (let i = 0; i < transcript_json.length; i++) {
+            let message = transcript_json[i];
+            // some intents have multiple intents, so we iterate through that as well
+            for (const intent of message.intents) {
+                if (!processedMap.has(intent)) {
+                    processedMap.set(intent, [1, new Map()])
+                }
+                else {
+                    const currList = processedMap.get(intent)
+                    const newIntentFreq = currList[0] + 1
+                    const sameAssociateMap = currList[1]
+
+                    processedMap.set(intent, [newIntentFreq, sameAssociateMap])
+                }
+                if (prevIntent !== undefined) {
+                    const currList = processedMap.get(prevIntent)
+                    let newAssociateMap = undefined
+                    if (currList[1] instanceof Map) {
+                        newAssociateMap = currList[1]
+                    }
+                    else {
+                        newAssociateMap = new Map(Object.entries(currList[1]))
+                    }
+                    if (!newAssociateMap.has(intent)) {
+                        newAssociateMap.set(intent, 1)
+                    }
+                    else {
+                        const newIntentAssociateFreq = newAssociateMap.get(intent) + 1
+                        newAssociateMap.set(intent, newIntentAssociateFreq)
+                    }
+                    currList[1] = newAssociateMap
+                }
+                prevIntent = intent
+            }
+        }
+    }
+    return processedMap
+}
+
+module.exports = processTranscript
+
