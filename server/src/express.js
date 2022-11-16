482a138dab08770444903d9b3d140c58f78f8580
Dhairya Khara <kharadhairya@gmail.com>
2022-11-16 00:44:15 -0500

    loading in all controllers

---
+++ server/src/express.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+
+const createUserController = require('./Controllers/createUser')
+const loginUserController = require('./Controllers/loginUser')
+const uploadTranscriptController = require('./Controllers/uploadTranscript')
+const getIntentsController = require('./Controllers/getIntents')
+const getOneTranscriptIntentsController = require('./Controllers/getOneTranscriptIntents')
+const getTranscriptsController = require('./Controllers/getTranscripts')
+
+const app = express()
+app.use(cors())
+app.use(createUserController)
+app.use(loginUserController)
+app.use(uploadTranscriptController)
+app.use(getIntentsController)
+app.use(getOneTranscriptIntentsController)
+app.use(getTranscriptsController)
+
+module.exports = app
