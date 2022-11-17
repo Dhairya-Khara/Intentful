22cb0d1ad9a03fb6e429eba01c24576d56d1e7a5
Aidan <aidan.li@mail.utoronto.ca>
2022-11-16 23:31:10 -0500

    renamed processTranscriptList

--- server/src/UseCases/TranscriptUseCases/intentIdentifierInteractor.js
+++ server/src/UseCases/TranscriptUseCases/intentIdentifierInteractor.js
@@ -3,7 +3,7 @@ const convertMultiWOZInteractor = require('./convertMultiWOZInteractor');
 
 const identifyIntents = (existingProcessedMap, transcript_json_list) => {
     const convertedTranscriptList = convertMultiWOZInteractor(transcript_json_list);
-    const allCurrentIntents = processTranscript(existingProcessedMap, convertedTranscriptList);
+    const allCurrentIntents = processTranscriptList(existingProcessedMap, convertedTranscriptList);
     return allCurrentIntents;
 }
 
