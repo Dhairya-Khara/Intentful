844c4ff570d5154bee3d56f32b21cc35077f08c2
BerkeAltiparmak <altiparmak.berke@gmail.com>
2022-11-16 19:04:19 -0500

    Now you can't upload transcripts with the same name ON THE BACKEND; Need to Fix FRONTEND
    When you try to upload transcripts of the same name, the backend refuses to upload and sends an error with the message "A transcript with the same name already exists". Frontend should fix the webpage so that the button to visualize a transcript appears ONLY IF no error is encountered on the backend. If the backend receives an error, the frontend should not put the visualize transcript button for that transcript.

--- server/src/UseCases/TranscriptUseCases/transcriptUploadInteractor.js
+++ server/src/UseCases/TranscriptUseCases/transcriptUploadInteractor.js
@@ -1,39 +1,59 @@
 const processTranscriptInteractor = require('./intentIdentifierInteractor')
 
 const uploadTranscriptInteractor = async (user, file, filename) => {
-    const json = JSON.parse(file)
-
-    let allCurrentIntents = new Map()
-
-    if (user.intents !== undefined) {
-        allCurrentIntents = new Map(Object.entries(user.intents));
-    }
-
-    //single transcript processing
-    let intentsForThisFile = processTranscriptInteractor(new Map(), [json])
-
-    //multiple transcript processing
-    allCurrentIntents = processTranscriptInteractor(allCurrentIntents, [json])
-
-    // save transcript
+    let transcriptNames = []
+    const existingTranscriptInfo = Object.entries(user.transcripts)
     try {
-        const obj = {}
-        obj[filename] = file
-        obj["intents"] = intentsForThisFile
-        user.transcripts = user.transcripts.concat(obj)
-        await user.save()
+        existingTranscriptInfo.forEach(info => {
+            transcriptNames.push(Object.entries(info[1])[0][0])
+            //info[1].forEach(subInfo => {
+                //transcriptNames.push(subInfo.keys()[0])})
+        })
+        console.log(transcriptNames)
     }
     catch (e) {
-        throw new Error("Error in saving transcripts", e)
+        throw new Error("Error in retrieving transcript names", e)
+    }
+    
+    if(!transcriptNames.includes(filename)) {
+        const json = JSON.parse(file)
+
+        let allCurrentIntents = new Map()
+
+        if (user.intents !== undefined) {
+            allCurrentIntents = new Map(Object.entries(user.intents));
+        }
+
+        //single transcript processing
+        let intentsForThisFile = processTranscriptInteractor(new Map(), [json])
+
+        //multiple transcript processing
+        allCurrentIntents = processTranscriptInteractor(allCurrentIntents, [json])
+
+        // save transcript
+        try {
+            const obj = {}
+            obj[filename] = file
+            obj["intents"] = intentsForThisFile
+            user.transcripts = user.transcripts.concat(obj)
+            await user.save()
+        }
+        catch (e) {
+            throw new Error("Error in saving transcripts", e)
+        }
+
+        // save intents
+        try {
+            user.intents = allCurrentIntents
+            await user.save()
+        }
+        catch (e) {
+            throw new Error("Error in saving intents", e)
+        }
     }
 
-    // save intents
-    try {
-        user.intents = allCurrentIntents
-        await user.save()
-    }
-    catch (e) {
-        throw new Error("Error in saving intents", e)
+    else {
+        throw new Error("A transcript with the same name already exists")
     }
 
 
