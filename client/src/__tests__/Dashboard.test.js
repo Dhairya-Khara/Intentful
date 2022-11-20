8e63859bdebe18542fc8bfc7127a5e94559e05ce
chenikabukes <chenika.bukes@mail.utoronto.ca>
2022-11-20 17:00:06 -0500

    babel.config.js added

--- client/src/__tests__/Dashboard.test.js
+++
@@ -1,8 +0,0 @@
-import Dashboard from '../pages/Dashboard/Dashboard'
-import { render, screen } from '@testing-library/react';
-
-test('renders the logout button', () => {
-  render(<Dashboard />);
-  
-  expect(screen.getByRole("heading")).toHaveTextContent(/Intentful/);
-});
--- client/src/__tests__/LandingPage.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import LandingPage from '../pages/LandingPage/LandingPage';
-
-test('renders the landing page', () => {
-  render(<LandingPage />);
-  
-  expect(screen.getByRole("heading")).toHaveTextContent(/Intentful/);
-});
