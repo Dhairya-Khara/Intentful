bac1a30410ce2d204f124ae5ce214ac8db2e1c25
Aidan <aidan.li@mail.utoronto.ca>
2022-11-20 01:06:10 -0500

    some progress on testing

--- server/jest-mongodb-config.js
+++ server/jest-mongodb-config.js
@@ -4,7 +4,7 @@ module.exports = {
             dbName: 'jest'
         },
         binary: {
-            version: '4.0.2', // Version of MongoDB
+            version: '6.0.1', // Version of MongoDB
             skipMD5: true
         },
         autoStart: false
