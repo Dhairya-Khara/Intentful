3d13379dbdfd72b6824b1d9b6084d8a0caa91fc8
Aidan <aidan.li@mail.utoronto.ca>
2022-11-21 10:37:53 -0500

    added comment: landing page test in progress

--- client/src/__tests__/LandingPage.test.js
+++ client/src/__tests__/LandingPage.test.js
@@ -1,8 +1,9 @@
-import {cleanup, fireEvent, render} from '@testing-library/react';
+import { cleanup, fireEvent, render } from '@testing-library/react';
 import LandingPage from '../pages/Dashboard/Dashboard';
 
+// in progress: test should fail
 test('renders learn react link', () => {
-    render(<LandingPage />);
-    const linkElement = screen.getHeading(/Intentful/i);
-    expect(linkElement).toEqual("Intentful");
-  });
+  render(<LandingPage />);
+  const linkElement = screen.getHeading(/Intentful/i);
+  expect(linkElement).toEqual("Intentful");
+});
