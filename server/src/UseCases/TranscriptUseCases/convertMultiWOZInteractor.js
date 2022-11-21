4da5773cd3c8b41f0e1a7afdd4eae1a4440da453
Aidan <aidan.li@mail.utoronto.ca>
2022-11-21 13:59:40 -0500

    added OCP comment

--- server/src/UseCases/TranscriptUseCases/convertMultiWOZInteractor.js
+++ server/src/UseCases/TranscriptUseCases/convertMultiWOZInteractor.js
@@ -1,4 +1,7 @@
 const multiWOZconverter = require('../utils/multiWOZconverter')
+// Follows open-closed principle: does not modify our existing transcript formats.
+// instead, in the intentIdentifierInteractor, we convert a MultiWOZ transcript to the 
+// existing "raw" format first before applying the processTranscript function from transcriptProcessor
 
 const convertMultiWOZ = (transcript_json_list) => {
     if (checkMultiWOZ === true) { // toggle multiWOZ with a button or something
