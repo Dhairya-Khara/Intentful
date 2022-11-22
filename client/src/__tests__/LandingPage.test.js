0457fc562a04857f1f7876aa2b36efba6314df7b
Aidan <aidan.li@mail.utoronto.ca>
2022-11-21 19:25:10 -0500

    removed LandingPage.test from PR: incomplete

--- client/src/__tests__/LandingPage.test.js
+++
@@ -1,9 +0,0 @@
-import { cleanup, fireEvent, render } from '@testing-library/react';
-import LandingPage from '../pages/Dashboard/Dashboard';
-
-// in progress: test should fail
-test('renders learn react link', () => {
-  render(<LandingPage />);
-  const linkElement = screen.getHeading(/Intentful/i);
-  expect(linkElement).toEqual("Intentful");
-});
