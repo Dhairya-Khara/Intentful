5a53bf142d834ba86267b5f18a4739d8ff2d3926
krishnacheemalapati <krishkrishct@gmail.com>
2022-11-23 17:03:02 -0500

    Added people's pictures

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -1,16 +1,12 @@
-import React from 'react'
-import Header from './../Sitewide/Header'
-import Footer from './../Sitewide/Footer'
-import Bios from './Bios';
-
+import React from "react";
+import Header from "./../Sitewide/Header";
+import Footer from "./../Sitewide/Footer";
+import Bios from "./Bios";
 
 export default function About() {
   return (
     <div className="AboutPage page">
       <Header />
-      <div className="create-text centered">
-      You're just one step away from unlocking the full potential of VoiceFlow for your company
-          </div>
       <Bios />
       <Footer />
     </div>
