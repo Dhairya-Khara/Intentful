2f1d0965c9099550b3831a3a8e62a485ea751fee
krishnacheemalapati <krishkrishct@gmail.com>
2022-11-23 15:53:30 -0500

    Finalized UI

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -1,12 +1,26 @@
+<<<<<<< HEAD
 import React from "react";
 import Header from "./../Sitewide/Header";
 import Footer from "./../Sitewide/Footer";
 import Bios from "./Bios";
+=======
+import React from 'react'
+import Header from './../Sitewide/Header'
+import Footer from './../Sitewide/Footer'
+import Bios from './Bios';
+
+>>>>>>> 5a82e2f (Finalized UI)
 
 export default function About() {
   return (
     <div className="AboutPage page">
       <Header />
+<<<<<<< HEAD
+=======
+      <div className="create-text centered">
+      You're just one step away from unlocking the full potential of VoiceFlow for your company
+          </div>
+>>>>>>> 5a82e2f (Finalized UI)
       <Bios />
       <Footer />
     </div>
--- client/src/pages/About/Bios.js
+++ client/src/pages/About/Bios.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { GitHub, Instagram, LinkedIn, Roles, Website } from "./Socials";
 import KrishnaPic from "./../Images/KrishnaPic.jpeg";
 import DhairyaPic from "./../Images/DhairyaPic.jpeg";
@@ -81,3 +82,82 @@ function Bios() {
 }
 
 export default Bios;
+=======
+import { GitHub, Instagram, LinkedIn, Roles, Website } from './Socials';
+import KrishnaPic from './../Images/Krishna.jped';
+import DhairyaPic from './../Images/Dhairya.png';
+import ChenikaPic from './../Images/Chenika.png';
+import BerkePic from './../Images/Berke.png';
+import AidanPic from './../Images/Aidan.png';
+
+function Bio(props) {
+	return (
+		<div className="card">
+			<img className="member-img" src={props.pic} alt={props.name} />
+			<h2 className="member-name">{props.name}</h2>
+			<h3 className="member-position">{props.positions}</h3>
+			<ul className="socials">{props.children}</ul>
+		</div>
+	);
+}
+
+function Execs() {
+	return (
+		<div className="row">
+			<Bio
+				name="Dhairya Khara" pic={DhairyaPic}
+				positions={[Roles.founder, Roles.chiefExec, Roles.teamLead]}
+			>
+                <Website>https://abyx.dev</Website>
+				<GitHub>DhairyaKhara</GitHub>
+				<LinkedIn>https://linkedin.com/in/tyler-jon-wong</LinkedIn>
+			</Bio>
+			<Bio
+				name="Krishna Cheemalapati" pic={KrishnaPic}
+				positions={[Roles.teamLead, Roles.frontend, Roles.backend]}
+			>
+				<Website>https://krishnacheemalapati.github.io</Website>
+				<GitHub>krishnacheemalapati</GitHub>
+				<LinkedIn>https://linkedin.com/in/krishnacheemalapati</LinkedIn>
+			</Bio>
+			<Bio
+				name="Berke Altiparmak" pic={BerkePic} positions={[Roles.founder, Roles.vp]}
+			>
+				<Website>https://shreyanshnair.wixsite.com/portfolio</Website>
+                <GitHub>Berke</GitHub>
+				<LinkedIn>https://linkedin.com/in/shreyansh-nair</LinkedIn>
+			</Bio>
+			<Bio
+				name="Aidan Li" pic={AidanPic}
+				positions={[Roles.treasurer, Roles.teamLead, Roles.frontend]}
+			>
+				<GitHub>Niwri</GitHub>
+				<LinkedIn>https://linkedin.com/in/irwin-ngo-9b306980</LinkedIn>
+			</Bio>
+			<Bio
+				name="Chenika Bukes" pic={ChenikaPic} positions={[Roles.secretary]}
+			>
+				<LinkedIn>https://linkedin.com/in/khondoker-intiser-alam-aa4417229</LinkedIn>
+				<Instagram>khondokerintiser</Instagram>
+			</Bio>
+		</div>
+	)
+}
+
+/**
+ * Construct the Meet the Team section.
+ */
+function Bios() {
+	return (
+		<div className="dev-bios">
+			<div className="inner">
+			<h1>Meet the Team</h1>
+			<h2 className="accent left">Executive Team</h2>
+			<Execs />
+			</div>
+		</div>
+	);
+}
+
+export default Bios;
+>>>>>>> 5a82e2f (Finalized UI)
