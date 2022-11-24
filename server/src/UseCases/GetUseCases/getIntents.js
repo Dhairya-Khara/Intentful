35b990e02faab6be1d9b6505fe39c985c9448f35
BerkeAltiparmak <altiparmak.berke@gmail.com>
2022-11-23 23:56:23 -0500

    Testing JSDoc for the first time

--- server/src/UseCases/GetUseCases/getIntents.js
+++ server/src/UseCases/GetUseCases/getIntents.js
@@ -1,3 +1,8 @@
+/**
+ * Returns the intents of the user
+ * @constructor
+ * @param {mongoose.Schema} user - The authorized user of the website
+ */
 const getIntentsInteractor = (user) =>{
     return user.intents
 }
