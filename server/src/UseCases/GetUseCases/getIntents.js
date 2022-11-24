4b333ba437437fa98043410f56d2bf34066fcb7e
BerkeAltiparmak <altiparmak.berke@gmail.com>
2022-11-24 02:06:02 -0500

    Modified doc of getIntents
    added return as well. We have to make sure the datatype of user.intents, right now I think it's Object

--- server/src/UseCases/GetUseCases/getIntents.js
+++ server/src/UseCases/GetUseCases/getIntents.js
@@ -1,7 +1,8 @@
 /**
  * Returns the intents of the user
  * @interactor
- * @param {mongoose.Schema} user - The authorized user of the website
+ * @param {mongoose.Schema} user - The current authorized user of the website
+ * @returns {Object} user.intents - intents identified in user's transcipts.
  */
 const getIntentsInteractor = (user) =>{
     return user.intents
