4b2022c2526f50c2cd360975a79a40714f9127c9
BerkeAltiparmak <altiparmak.berke@gmail.com>
2022-11-27 23:24:04 -0500

    Documenting intentIdentifierInteractor

--- server/src/UseCases/TranscriptUseCases/intentIdentifierInteractor.js
+++ server/src/UseCases/TranscriptUseCases/intentIdentifierInteractor.js
@@ -1,5 +1,14 @@
 const processTranscript = require('../utils/transcriptProcessor')
 
+/**
+ * Returns a map of all processed transcripts and their identified intents
+ * by processing newly uploaded transcripts and merging the resulting map
+ * with the already existing one.
+ * @interactor
+ * @param {Object} existingProcessedMap - Map of already processed transcripts with their intents.
+ * @param {Array} transcript_json_list - List of transcripts to be uploaded
+ * @return {Map} allCurrentIntents - Map of all processed transcripts (uploaded by user) and their intents.
+ */
 const identifyIntents = (existingProcessedMap, transcript_json_list) =>{
     const allCurrentIntents = processTranscript(existingProcessedMap, transcript_json_list)
     return allCurrentIntents
