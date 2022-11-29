aee0f5bc7ad2b8007b685f086a4f9a3acb78b317
BerkeAltiparmak <altiparmak.berke@gmail.com>
2022-11-28 23:50:16 -0500

    Styling getOneTranscriptIntentsInteractor
    I've noticed a lack of consistency in styles files, need to fix that next.

--- server/src/UseCases/GetUseCases/getIntentsInteractor.js
+++ server/src/UseCases/GetUseCases/getIntentsInteractor.js
@@ -1,5 +1,5 @@
 /**
- * Returns the intents of the user
+ * Returns the intents of the user.
  * @interactor
  * @param {mongoose.Schema} user - The current authorized user of the website
  * @returns {Object} user.intents - intents identified in user's transcipts.
--- server/src/UseCases/GetUseCases/getOneTranscriptIntentsInteractor.js
+++ server/src/UseCases/GetUseCases/getOneTranscriptIntentsInteractor.js
@@ -1,3 +1,10 @@
+/**
+ * Returns the intents of the single transcript requested by the user.
+ * @interactor
+ * @param {mongoose.Schema} user - The current authorized user of the website.
+ * @param {String} name - Name of the transcript file requested by the user.
+ * @returns {Object} user.intents - The intents identified in user's transcipts.
+ */
 const getOneTranscriptIntentsInteractor = (user, name) =>{
     const transcript = user.transcripts
     for(const obj of transcript){
