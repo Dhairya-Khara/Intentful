7270c484b831beb34ca9754bb769a06dc9130fa0
Aidan <aidan.li@mail.utoronto.ca>
2022-11-30 00:00:17 -0500

    new approach: logic in convertMultiWOZInteractor

--- server/src/Controllers/transcriptUploadController.js
+++ server/src/Controllers/transcriptUploadController.js
@@ -9,23 +9,15 @@ const upload = multer()
 router.post('/uploadTranscript', auth, upload.single('file'), async (req, res) => {
     const user = req.user
     const file = req.file.buffer
+    const filename = req.file.originalname
 
-    let convertedTranscriptList = convertMultiWOZInteractor(file); // 
-    console.log(convertedTranscriptList)
-    console.log(typeof convertedTranscriptList)
-    console.log(convertedTranscriptList.length)
-    // for (let i = 0; i < convertedTranscriptList.length; i++) {
-    //     const curr_transcript = convertedTranscriptList[i]
-    //     //console.log(i)
-    //     const filename = req.file.originalname + `_transcript_${i}`
-    //     try {
-    //         await uploadTranscriptInteractor(user, curr_transcript, filename)
-    //         // res.sendStatus(200)
-    //     }
-    //     catch (e) {
-    //         res.status(422).send(e.message)
-    //     }
-    // }
+    try {
+        await convertMultiWOZInteractor(user, file, filename)
+        // res.sendStatus(200)
+    }
+    catch (e) {
+        res.status(422).send(e.message)
+    }
     res.sendStatus(200)   // if we made it here without an error, we good
 })
 
--- server/src/UseCases/TranscriptUseCases/convertMultiWOZInteractor.js
+++ server/src/UseCases/TranscriptUseCases/convertMultiWOZInteractor.js
@@ -4,12 +4,32 @@ const sampleDialogue = require('./sample transcripts/size3_dialogues_001.json')
 // instead, in the intentIdentifierInteractor, we convert a MultiWOZ transcript to the 
 // existing "raw" format first before applying the processTranscript function from transcriptProcessor
 
-const convertMultiWOZInteractor = (transcript_json_list) => {
-    if (true) { // toggle multiWOZ with a button or something
-        let convertedMultiWOZlist = multiWOZconverter(transcript_json_list)
-        return convertedMultiWOZlist
+const convertMultiWOZInteractor = async (user, file, filename) => {
+    if (user.transcripts === undefined || user.email === undefined) { throw new Error("Not a valid user") }
+    console.log(file)
+    console.log(typeof file)
+    console.log(file.length)
+    let convertedMultiWOZtoOrigList = multiWOZconverter(file);
+    console.log(convertedMultiWOZtoOrigList)
+    console.log(typeof convertedMultiWOZtoOrigList)
+    console.log(convertedMultiWOZtoOrigList.length)
+    for (let i = 0; i < convertedTranscriptList.length; i++) {
+        const currTranscript = convertedTranscriptList[i]
+        const currTranscriptFilename = filename + `_transcript_${i}`
+        try {
+            await uploadTranscriptInteractor(user, currTranscript, currTranscriptFilename)
+        }
+        catch (e) {
+            throw new Error("Error in saving a transcript", e)
+        }
     }
 }
+// const convertMultiWOZInteractor = (transcript_json_list) => {
+//     if (true) { // toggle multiWOZ with a button or something
+//         let convertedMultiWOZlist = multiWOZconverter(transcript_json_list)
+//         return convertedMultiWOZlist
+//     }
+// }
 
 // console.log(convertMultiWOZInteractor(sampleDialogue))
 // console.log(convertMultiWOZInteractor(sampleDialogue).length)
