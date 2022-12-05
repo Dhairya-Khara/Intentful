7bad94b7d6df9c9e5eb0c78ffafd7e11bcebcbe8
Aidan <aidan.li@mail.utoronto.ca>
2022-12-05 00:11:36 -0500

    updated transcript use-case tests

---
+++ server/src/__tests__/UseCases/TranscriptUseCases/transcriptProcessInteractor.test.js
@@ -0,0 +1,44 @@
+const transcriptProcessInteractor = require('../../../UseCases/TranscriptUseCases/transcriptProcessInteractor')
+const sampleTranscript = require('../../sample transcripts/transcript1.json')
+const sampleTranscript2 = require('../../sample transcripts/transcript2.json')
+const sampleTranscript3 = require('../../sample transcripts/transcript3.json')
+
+describe('processed sample transcript', () => {
+    test('has correct values for one transcript', () => {
+        const processedTranscript = new Map([
+            ['find_restaurant', [3, new Map([['find_hotel', 3]])]],
+            ['find_hotel', [4, new Map([['find_restaurant', 2], ['book_hotel', 1]])]],
+            ['book_hotel', [1, new Map()]]
+        ]);
+        expect(transcriptProcessInteractor(new Map(), [sampleTranscript])).toEqual(processedTranscript)
+    })
+
+    test('has correct values for multiple transcripts', () => {
+        const processedTranscript = new Map([
+            ['find_restaurant', [5, new Map([['find_hotel', 4], ['book_hotel', 1]])]],
+            ['find_hotel', [8, new Map([['find_restaurant', 4], ['book_hotel', 2]])]],
+            ['book_hotel', [4, new Map()]],
+            ['find_train', [3, new Map([['find_hotel', 1]])]]
+        ]);
+        expect(transcriptProcessInteractor(new Map(), [sampleTranscript,
+            sampleTranscript2, sampleTranscript3])).toEqual(processedTranscript)
+    })
+
+    test('processes transcripts and adds them correctly to itself', () => {
+        const existingMap = new Map([
+            ['find_restaurant', [3, new Map([['find_hotel', 1]])]],
+            ['find_hotel', [2, new Map([['book_hotel', 1]])]],
+            ['book_hotel', [1, new Map()]]
+        ]);
+
+        const processedTranscript = new Map([
+            ['find_restaurant', [5, new Map([['find_hotel', 2], ['book_hotel', 1]])]],
+            ['find_hotel', [6, new Map([['book_hotel', 2], ['find_restaurant', 2]])]],
+            ['book_hotel', [4, new Map()]],
+            ['find_train', [3, new Map([['find_hotel', 1]])]]
+        ]);
+
+        expect(transcriptProcessInteractor(existingMap,
+            [sampleTranscript2, sampleTranscript3])).toEqual(processedTranscript)
+    })
+})
