c398dcf8a2fbf228fb217efc44948e95a48525be
BerkeAltiparmak <altiparmak.berke@gmail.com>
2022-12-07 18:16:52 -0500

    Updating UserSchema Clean Architecture explanation

--- server/src/Entities/UserSchema.js
+++ server/src/Entities/UserSchema.js
@@ -2,23 +2,29 @@ const mongoose = require('mongoose')
 const TranscriptSchema = require('./Transcript')
 
 // TEAM DISCUSSION re: CLEAN ARCHITECTURE
+
 // We understand that Entities technically should be independent of frameworks like Mongoose.
-// If we have more time, we might want to implement our User entity as a Mongoose schema inside 
-// a Data Persistence layer, and maybe have our Entity be an abstract class instead.
+// Another, maybe more cleaner, approach would be implementing our User entity
+// as a Mongoose schema inside a Data Persistence layer, which would allow us
+// to keep Entity as an abstract class instead. Apart from this, however,
+// we believe our approach is a great practice of Clean Architecture, requiring
+// developer to change only a single file to change any behavior of the app,
+// and thus conforming to all Uncle Bob's rules.
 
 // However, since our app is specified to be a MERN app, and given the scope of our project,
 // it makes little practical sense to spend a significant time rewriting our whole backend to
 // add in a layer specifically for database implementations. Switching database implementations
 // would be unlikely in the future due to how well MERN works together.
 
-let Schema = new mongoose.Schema({
+let UserSchema = new mongoose.Schema({
     email: {
         type: String
     },
     password: {
         type: String
     },
-    transcripts: [TranscriptSchema],
+
+    transcripts: [TranscriptSchema],  //transcript is an entity of its own.
 
     intents: {
 
@@ -28,7 +34,7 @@ let Schema = new mongoose.Schema({
 
 
 
-let User = mongoose.model('User', Schema)
+let User = mongoose.model('User', UserSchema)
 
 
 module.exports = User
